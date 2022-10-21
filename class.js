1ebbb26165efe09e206d6e514c8ccdb9797e71c8
Audyssee <audrey.gentili38210@gmail.com>
2022-10-21 11:47:52 +0200

    commit

--- class.js
+++ class.js
@@ -1,273 +1,272 @@
 class Piece {
-    constructor(nom, nbpieces, orientation, col) {
-      this.nom = nom;
-      this.nbpieces = nbpieces;
-      this.orientation = orientation;
-      this.col = col;
-    }
-  }
-  
-  class Piece3 extends Piece {
-    constructor(nom, nbpieces, orientation, col, p1, p2, p3) {
-      super(nom, nbpieces, orientation, col);
-      this.p1 = p1;
-      this.p2 = p2;
-      this.p3 = p3;
-    }
-    
-    afficher() {
-      fill(this.col);
-      var p = [this.p1, this.p2, this.p3];
-      for (let i = 0; i < p.length; i++) {
-        if (p[i].length == 4) {
-          rect(p[i][0], p[i][1], p[i][2], p[i][3]);
-        }
-        if (p[i].length == 6) {
-          triangle(p[i][0], p[i][1], p[i][2], p[i][3], p[i][4], p[i][5]);
-        }
-      }
-    }
-    
-    rotationL() {
-    }
-    
-    rotationR() {
-    }
-    
-    miroir() {
-      
-    }
-    
-    deplacement(bx, by) {
-        print("bx:"+bx+" by:"+by);
-        var x = mouseX;
-        var y = mouseY;
-        print("x:"+x+" y:"+y);
-        var p = [this.p1, this.p2, this.p3];
-        for (let i = 0; i < p.length; i++) {
-            if (p[i].length == 4) {
-            p[i][0] += x-bx;
-            p[i][1] = p[i][1]+(y-by);
-            }
-            if (p[i].length == 6) {
-            p[i][0] = p[i][0]+(x-bx);
-            p[i][1] = p[i][1]+(y-by);
-            p[i][2] = p[i][2]+(x-bx);
-            p[i][3] = p[i][3]+(y-by);
-            p[i][4] = p[i][4]+(x-bx);
-            p[i][5] = p[i][5]+(y-by);
-            }
-        }
-    }
-  }
-  
-  class Piece4 extends Piece {
-    constructor(nom, nbpieces, orientation, col, p1, p2, p3, p4) {
-      super(nom, nbpieces, orientation, col);
-      this.p1 = p1;
-      this.p2 = p2;
-      this.p3 = p3;
-      this.p4 = p4;
-    }
-    
-    afficher() {
-      fill(this.col);
-      var p = [this.p1, this.p2, this.p3, this.p4];
-      for (let i = 0; i < p.length; i++) {
-        if (p[i].length == 4) {
-          rect(p[i][0], p[i][1], p[i][2], p[i][3]);
-        }
-        if (p[i].length == 6) {
-          triangle(p[i][0], p[i][1], p[i][2], p[i][3], p[i][4], p[i][5]);
-        }
-      }
-    }
-    
-    rotationL() {
-      if (this.orientation == "o") {
-        this.orientation = "s";
-        this.p1[0] += 125;
-        this.p1[1] += 250;
-        this.p1[2] += 125;
-        this.p1[3] += 0;
-        this.p1[4] += 0;
-        this.p1[5] += 125;
-        this.p3[0] += -125;
-        this.p3[1] += -125;
-        this.p4[0] += -250;
-        this.p4[1] += 0;
-      } else if (this.orientation == "s") {
-        this.orientation = "e";
-        this.p1[0] += 250;
-        this.p1[1] += -125;
-        this.p1[2] += 0;
-        this.p1[3] += -125;
-        this.p1[4] += 125;
-        this.p1[5] += 0;
-        this.p3[0] += -125;
-        this.p3[1] += 125;
-        this.p4[0] += 0;
-        this.p4[1] += 250;
-      }  else if (this.orientation == "e") {
-        this.orientation = "n";
-        this.p1[0] += -125;
-        this.p1[1] += -250;
-        this.p1[2] += -125;
-        this.p1[3] += 0;
-        this.p1[4] += 0;
-        this.p1[5] += -125;
-        this.p3[0] += 125;
-        this.p3[1] += 125;
-        this.p4[0] += 250;
-        this.p4[1] += 0;
-      } else if (this.orientation == "n") {
-        this.orientation = "o"; 
-        this.p1[0] += -250;
-        this.p1[1] += 125;
-        this.p1[2] += 0;
-        this.p1[3] += 125;
-        this.p1[4] += -125;
-        this.p1[5] += 0;
-        this.p3[0] += 125;
-        this.p3[1] += -125;
-        this.p4[0] += 0;
-        this.p4[1] += -250;        
-      }
-    }
-  
-    rotationR() {
-      if (this.orientation == "o") {
-        this.orientation = "n";
-        this.p1[0] += 250;
-        this.p1[1] += -125;
-        this.p1[2] += 0;
-        this.p1[3] += -125;
-        this.p1[4] += 125;
-        this.p1[5] += 0;
-        this.p3[0] += -125;
-        this.p3[1] += 125;
-        this.p4[0] += 0;
-        this.p4[1] += 250;
-      } else if (this.orientation == "n") {
-        this.orientation = "e";
-        this.p1[0] += 125;
-        this.p1[1] += 250;
-        this.p1[2] += 125;
-        this.p1[3] += 0;
-        this.p1[4] += 0;
-        this.p1[5] += 125;
-        this.p3[0] += -125;
-        this.p3[1] += -125;
-        this.p4[0] += -250;
-        this.p4[1] += 0;
-      }  else if (this.orientation == "e") {
-        this.orientation = "s";
-        this.p1[0] += -250;
-        this.p1[1] += 125;
-        this.p1[2] += 0;
-        this.p1[3] += 125;
-        this.p1[4] += -125;
-        this.p1[5] += 0;
-        this.p3[0] += 125;
-        this.p3[1] += -125;
-        this.p4[0] += 0;
-        this.p4[1] += -250;
-      } else if (this.orientation == "s") {
-        this.orientation = "o"; 
-        this.p1[0] += -125;
-        this.p1[1] += -250;
-        this.p1[2] += -125;
-        this.p1[3] += 0;
-        this.p1[4] += 0;
-        this.p1[5] += -125;
-        this.p3[0] += 125;
-        this.p3[1] += 125;
-        this.p4[0] += 250;
-        this.p4[1] += 0;        
-      }
-    }
-    
-    miroir() {
-      
-    }
-    
-    deplacement(bx, by) {
-        print("bx:"+bx+" by:"+by);
-        var x = mouseX;
-        var y = mouseY;
-        print("x:"+x+" y:"+y);
-        var p = [this.p1, this.p2, this.p3, this.p4];
-        for (let i = 0; i < p.length; i++) {
-            if (p[i].length == 4) {
-                p[i][0] += x-bx;
-                p[i][1] = p[i][1]+(y-by);
-            }
-            if (p[i].length == 6) {
-                p[i][0] = p[i][0]+(x-bx);
-                p[i][1] = p[i][1]+(y-by);
-                p[i][2] = p[i][2]+(x-bx);
-                p[i][3] = p[i][3]+(y-by);
-                p[i][4] = p[i][4]+(x-bx);
-                p[i][5] = p[i][5]+(y-by);
-            }
-        }
-    }
-  }
-  
-  
-  
-  class Piece5 extends Piece {
-    constructor(nom, nbpieces, orientation, col, p1, p2, p3, p4, p5) {
-      super(nom, nbpieces, orientation, col);
-      this.p1 = p1;
-      this.p2 = p2;
-      this.p3 = p3;
-      this.p4 = p4;
-      this.p5 = p5;
-    }
-    
-    afficher() {
-      fill(this.col);
-      var p = [this.p1, this.p2, this.p3, this.p4, this.p5];
-      for (let i = 0; i < p.length; i++) {
-        if (p[i].length == 4) {
-          rect(p[i][0], p[i][1], p[i][2], p[i][3]);
-        }
-        if (p[i].length == 6) {
-          triangle(p[i][0], p[i][1], p[i][2], p[i][3], p[i][4], p[i][5]);
-        }
-      }
-    }
-    
-    rotation() {
-      
-    }
-    
-    miroir() {
-      
-    }
-    
-    deplacement(bx, by) {
-        print("bx:"+bx+" by:"+by);
-        var x = mouseX;
-        var y = mouseY;
-        print("x:"+x+" y:"+y);
-        var p = [this.p1, this.p2, this.p3, this.p4, this.p5];
-        for (let i = 0; i < p.length; i++) {
-            if (p[i].length == 4) {
-                p[i][0] += x-bx;
-                p[i][1] = p[i][1]+(y-by);
-            }
-            if (p[i].length == 6) {
-                p[i][0] = p[i][0]+(x-bx);
-                p[i][1] = p[i][1]+(y-by);
-                p[i][2] = p[i][2]+(x-bx);
-                p[i][3] = p[i][3]+(y-by);
-                p[i][4] = p[i][4]+(x-bx);
-                p[i][5] = p[i][5]+(y-by);
-            }
-        }
-    }
-  }
-  
-  
+	constructor(nom, nbpieces, orientation, col) {
+		this.nom = nom;
+		this.nbpieces = nbpieces;
+		this.orientation = orientation;
+		this.col = col;
+	}
+}
+	
+class Piece3 extends Piece {
+	constructor(nom, nbpieces, orientation, col, p1, p2, p3) {
+		super(nom, nbpieces, orientation, col);
+		this.p1 = p1;
+		this.p2 = p2;
+		this.p3 = p3;
+	}
+	
+	afficher() {
+		fill(this.col);
+		var p = [this.p1, this.p2, this.p3];
+		for (let i = 0; i < p.length; i++) {
+			if (p[i].length == 4) {
+				rect(p[i][0], p[i][1], p[i][2], p[i][3]);
+			}
+			if (p[i].length == 6) {
+				triangle(p[i][0], p[i][1], p[i][2], p[i][3], p[i][4], p[i][5]);
+			}
+		}
+	}
+	
+	rotationL() {
+	}
+	
+	rotationR() {
+	}
+	
+	miroir() {
+		
+	}
+	
+	deplacement(bx, by) {
+		print("bx:"+bx+" by:"+by);
+		var x = mouseX;
+		var y = mouseY;
+		print("x:"+x+" y:"+y);
+		var p = [this.p1, this.p2, this.p3];
+		for (let i = 0; i < p.length; i++) {
+			if (p[i].length == 4) {
+				p[i][0] += x-bx;
+				p[i][1] = p[i][1]+(y-by);
+			}
+			if (p[i].length == 6) {
+				p[i][0] = p[i][0]+(x-bx);
+				p[i][1] = p[i][1]+(y-by);
+				p[i][2] = p[i][2]+(x-bx);
+				p[i][3] = p[i][3]+(y-by);
+				p[i][4] = p[i][4]+(x-bx);
+				p[i][5] = p[i][5]+(y-by);
+			}
+		}
+	}
+}
+
+class Piece4 extends Piece {
+	constructor(nom, nbpieces, orientation, col, p1, p2, p3, p4) {
+		super(nom, nbpieces, orientation, col);
+		this.p1 = p1;
+		this.p2 = p2;
+		this.p3 = p3;
+		this.p4 = p4;
+	}
+	
+	afficher() {
+		fill(this.col);
+		var p = [this.p1, this.p2, this.p3, this.p4];
+		for (let i = 0; i < p.length; i++) {
+			if (p[i].length == 4) {
+				rect(p[i][0], p[i][1], p[i][2], p[i][3]);
+			}
+			if (p[i].length == 6) {
+				triangle(p[i][0], p[i][1], p[i][2], p[i][3], p[i][4], p[i][5]);
+			}
+		}
+	}
+	
+	rotationL() {
+		if (this.orientation == "o") {
+			this.orientation = "s";
+			this.p1[0] += 125;
+			this.p1[1] += 250;
+			this.p1[2] += 125;
+			this.p1[3] += 0;
+			this.p1[4] += 0;
+			this.p1[5] += 125;
+			this.p3[0] += -125;
+			this.p3[1] += -125;
+			this.p4[0] += -250;
+			this.p4[1] += 0;
+		} else if (this.orientation == "s") {
+			this.orientation = "e";
+			this.p1[0] += 250;
+			this.p1[1] += -125;
+			this.p1[2] += 0;
+			this.p1[3] += -125;
+			this.p1[4] += 125;
+			this.p1[5] += 0;
+			this.p3[0] += -125;
+			this.p3[1] += 125;
+			this.p4[0] += 0;
+			this.p4[1] += 250;
+		}  else if (this.orientation == "e") {
+			this.orientation = "n";
+			this.p1[0] += -125;
+			this.p1[1] += -250;
+			this.p1[2] += -125;
+			this.p1[3] += 0;
+			this.p1[4] += 0;
+			this.p1[5] += -125;
+			this.p3[0] += 125;
+			this.p3[1] += 125;
+			this.p4[0] += 250;
+			this.p4[1] += 0;
+		} else if (this.orientation == "n") {
+			this.orientation = "o"; 
+			this.p1[0] += -250;
+			this.p1[1] += 125;
+			this.p1[2] += 0;
+			this.p1[3] += 125;
+			this.p1[4] += -125;
+			this.p1[5] += 0;
+			this.p3[0] += 125;
+			this.p3[1] += -125;
+			this.p4[0] += 0;
+			this.p4[1] += -250;        
+		}
+	}
+
+	rotationR() {
+		if (this.orientation == "o") {
+			this.orientation = "n";
+			this.p1[0] += 250;
+			this.p1[1] += -125;
+			this.p1[2] += 0;
+			this.p1[3] += -125;
+			this.p1[4] += 125;
+			this.p1[5] += 0;
+			this.p3[0] += -125;
+			this.p3[1] += 125;
+			this.p4[0] += 0;
+			this.p4[1] += 250;
+		} else if (this.orientation == "n") {
+			this.orientation = "e";
+			this.p1[0] += 125;
+			this.p1[1] += 250;
+			this.p1[2] += 125;
+			this.p1[3] += 0;
+			this.p1[4] += 0;
+			this.p1[5] += 125;
+			this.p3[0] += -125;
+			this.p3[1] += -125;
+			this.p4[0] += -250;
+			this.p4[1] += 0;
+		}  else if (this.orientation == "e") {
+			this.orientation = "s";
+			this.p1[0] += -250;
+			this.p1[1] += 125;
+			this.p1[2] += 0;
+			this.p1[3] += 125;
+			this.p1[4] += -125;
+			this.p1[5] += 0;
+			this.p3[0] += 125;
+			this.p3[1] += -125;
+			this.p4[0] += 0;
+			this.p4[1] += -250;
+		} else if (this.orientation == "s") {
+			this.orientation = "o"; 
+			this.p1[0] += -125;
+			this.p1[1] += -250;
+			this.p1[2] += -125;
+			this.p1[3] += 0;
+			this.p1[4] += 0;
+			this.p1[5] += -125;
+			this.p3[0] += 125;
+			this.p3[1] += 125;
+			this.p4[0] += 250;
+			this.p4[1] += 0;        
+		}
+	}
+	
+	miroir() {
+		
+	}
+	
+	deplacement(bx, by) {
+		print("bx:"+bx+" by:"+by);
+		var x = mouseX;
+		var y = mouseY;
+		print("x:"+x+" y:"+y);
+		var p = [this.p1, this.p2, this.p3, this.p4];
+		for (let i = 0; i < p.length; i++) {
+			if (p[i].length == 4) {
+				p[i][0] += x-bx;
+				p[i][1] = p[i][1]+(y-by);
+			}
+			if (p[i].length == 6) {
+				p[i][0] = p[i][0]+(x-bx);
+				p[i][1] = p[i][1]+(y-by);
+				p[i][2] = p[i][2]+(x-bx);
+				p[i][3] = p[i][3]+(y-by);
+				p[i][4] = p[i][4]+(x-bx);
+				p[i][5] = p[i][5]+(y-by);
+			}
+		}
+	}
+}
+
+
+
+class Piece5 extends Piece {
+	constructor(nom, nbpieces, orientation, col, p1, p2, p3, p4, p5) {
+		super(nom, nbpieces, orientation, col);
+		this.p1 = p1;
+		this.p2 = p2;
+		this.p3 = p3;
+		this.p4 = p4;
+		this.p5 = p5;
+	}
+	
+	afficher() {
+		fill(this.col);
+		var p = [this.p1, this.p2, this.p3, this.p4, this.p5];
+		for (let i = 0; i < p.length; i++) {
+			if (p[i].length == 4) {
+				rect(p[i][0], p[i][1], p[i][2], p[i][3]);
+			}
+			if (p[i].length == 6) {
+				triangle(p[i][0], p[i][1], p[i][2], p[i][3], p[i][4], p[i][5]);
+			}
+		}
+	}
+	
+	rotation() {
+		
+	}
+	
+	miroir() {
+		
+	}
+	
+	deplacement(bx, by) {
+		print("bx:"+bx+" by:"+by);
+		var x = mouseX;
+		var y = mouseY;
+		print("x:"+x+" y:"+y);
+		var p = [this.p1, this.p2, this.p3, this.p4, this.p5];
+		for (let i = 0; i < p.length; i++) {
+			if (p[i].length == 4) {
+				p[i][0] += x-bx;
+				p[i][1] = p[i][1]+(y-by);
+			}
+			if (p[i].length == 6) {
+				p[i][0] = p[i][0]+(x-bx);
+				p[i][1] = p[i][1]+(y-by);
+				p[i][2] = p[i][2]+(x-bx);
+				p[i][3] = p[i][3]+(y-by);
+				p[i][4] = p[i][4]+(x-bx);
+				p[i][5] = p[i][5]+(y-by);
+			}
+		}
+	}
+}
+
--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 var mirror_checked = false;
-var selected = 1;
+var selected = null;
 var pieces = [];
 var basex, basey;
 
@@ -40,42 +40,54 @@ function setup() {
 }
 
 function selectedPiece() {
-  sX = mouseX;
-  sY = mouseY;
-  for (let i = 0; i < pieces.length; i++) {
-	if(sX >= pieces[i].p2[0] && sX <= pieces[i].p2[0] + 125 && sY >= pieces[i].p2[1] && sY <= pieces[i].p2[1] + 125) {
-	  selected = i+1;
-	  print(selected);
+	sX = mouseX;
+	sY = mouseY;
+	for (let i = 0; i < pieces.length; i++) {
+		if (sX >= pieces[i].p2[0] && sX <= pieces[i].p2[0] + 125 && sY >= pieces[i].p2[1] && sY <= pieces[i].p2[1] + 125) {
+			selected = i+1;
+			print(selected);
+		}
 	}
-  }
 }
-  
+
 function mousePressed() {
-  selectedPiece();
-  basex = mouseX;
-  basey = mouseY;
+	selectedPiece();
+	basex = mouseX;
+	basey = mouseY;
 }
 
 function movePiece(x, y) {
-  for (let i = 0; i < pieces.length; i++) {
-	if (pieces[i].nom == selected) {
-	  pieces[i].deplacement(x, y);
+	for (let i = 0; i < pieces.length; i++) {
+		if (pieces[i].nom == selected) {
+			pieces[i].deplacement(x, y);
+		}
 	}
-  }
 }
-  
+
 function mouseDragged() {
-  if (mirror_checked == false && mouseX > 50 && mouseX < 750 && mouseY > 50 && mouseY < 625) {
-	basex = mouseX-basex;
-	basey = mouseY-basey;
-	movePiece(basex, basey);
-  }
+	if (mirror_checked == false && mouseX > 50 && mouseX < 750 && mouseY > 50 && mouseY < 625) {
+		basex = mouseX-basex;
+		basey = mouseY-basey;
+		movePiece(basex, basey);
+	}
+}
+
+function clipPiece() {
+	sX = mouseX;
+	sY = mouseY;
+}
+
+function mouseReleased() {
+	print('release');
+	clipPiece();
+	pieceSelected = selected;
+	selected = null;
 }
 
 function rotationR() {
 	for (let i = 0; i < pieces.length; i++) {
 		if (pieces[i].nom == selected) {
-		  pieces[i].rotationR();
+			pieces[i].rotationR();
 		}
 	}
 }
@@ -83,92 +95,100 @@ function rotationR() {
 function rotationL() {
 	for (let i = 0; i < pieces.length; i++) {
 		if (pieces[i].nom == selected) {
-		  pieces[i].rotationL();
+			pieces[i].rotationL();
 		}
 	}
 }
 
 function keyPressed() {
-  if (keyCode === LEFT_ARROW) {
-    rotationL();
-  } else if (keyCode === RIGHT_ARROW) {
-    rotationR();
-  }
+	if (keyCode === LEFT_ARROW) {
+		rotationL();
+	} else if (keyCode === RIGHT_ARROW) {
+		rotationR();
+	}
 }
 
 function draw() { 
-  fill(0);
-  strokeWeight(1);
-  rect(80,80,750,625);
-  strokeWeight(1);
-  fill(255);
-  stroke(0); 
-  //canva
-  for( i = 1; i <= 6;i++){
-	for( j=1; j <= 5; j++){
-	  rect(i*125-45, j*125-45, 125,125);
+	fill(0);
+	strokeWeight(1);
+	rect(80,80,750,625);
+	strokeWeight(1);
+	fill(255);
+	stroke(0); 
+	//canva
+	for(i = 1; i <= 6;i++) {
+		for(j=1; j <= 5; j++) {
+			rect(i*125-45, j*125-45, 125,125);
+		}
 	}
-  }
-  
-  textSize(32);
-  text("Grab pieces on their center",270, 40);
-  
-  rotate_left = createButton("rotate left <--");
-  rotate_left.position(50,800);
-  rotate_left.size(200,100);
-  rotate_left.mousePressed(rotationL);
 
-  rotate_right = createButton("rotate right -->");
-  rotate_right.position(350,800);
-  rotate_right.size(200,100);
-  rotate_right.mousePressed(rotationR)
-  
-  mirror = createButton("|| mirror ||");
-  mirror.position(650,800);
-  mirror.size(200,100);
-  mirror.mousePressed();
+	textSize(32);
+	text("Grab pieces on their center",270, 40);
 
-  piece1.afficher();
-  
-  /*
-  //piece verte claire
-  fill('rgba(134,207,11,0.8)');
-  triangle(80,205,205,330,205,205);
-  rect(205,205,125,125);
-  rect(330,205,125,125);
-  rect(330,80,125,125);
-  
-  //piece bleue foncée
+	rotate_left = createButton("rotate left <--");
+	rotate_left.position(50,800);
+	rotate_left.size(200,100);
+	rotate_left.mousePressed(rotationL);
 
-  fill('rgba(3,13,182,0.8)');
-  triangle(80,205,205,330,205,205);
-  rect(205,205,125,125);
-  rect(330,205,125,125);
-  triangle(330,205,455,80,455,205);
-  
-  //piece jaune
+	rotate_right = createButton("rotate right -->");
+	rotate_right.position(350,800);
+	rotate_right.size(200,100);
+	rotate_right.mousePressed(rotationR)
 
-  fill('rgba(235,238,24,0.8)');
-  triangle(80,205,205,330,205,205);
-  rect(205,205,125,125);
-  rect(330,205,125,125);
-  triangle(330,330,330,455,455,330);
-  
-  //piece orange
+	mirror = createButton("|| mirror ||");
+	mirror.position(650,800);
+	mirror.size(200,100);
+	mirror.mousePressed();
 
-  fill('rgba(214,122,21,0.8)');
-  triangle(205,330,330,455,330,330);
-  rect(205,205,125,125);
-  rect(330,330,125,125);
-  triangle(330,330,330,205,455,330);
-  triangle(455,330,455,455,580,455);
-  
-  //piece violette
+	if (mirror_checked) {
+		rotate_left.hide();
+		rotate_right.hide();
+	  } else {
+		rotate_left.show();
+		rotate_right.show();
+	  }
 
-  fill('rgba(88,0,138,0.8)');
-  triangle(205,330,330,205,330,330);
-  triangle(205,330,330,455,330,330);
-  rect(330,330,125,125);
-  triangle(330,330,330,205,455,330);
-  triangle(455,330,455,455,580,455); */
+	piece1.afficher();
 }
+
+/*
+//piece verte claire
+fill('rgba(134,207,11,0.8)');
+triangle(80,205,205,330,205,205);
+rect(205,205,125,125);
+rect(330,205,125,125);
+rect(330,80,125,125);
+
+//piece bleue foncée
+
+fill('rgba(3,13,182,0.8)');
+triangle(80,205,205,330,205,205);
+rect(205,205,125,125);
+rect(330,205,125,125);
+triangle(330,205,455,80,455,205);
+
+//piece jaune
+
+fill('rgba(235,238,24,0.8)');
+triangle(80,205,205,330,205,205);
+rect(205,205,125,125);
+rect(330,205,125,125);
+triangle(330,330,330,455,455,330);
+
+//piece orange
+
+fill('rgba(214,122,21,0.8)');
+triangle(205,330,330,455,330,330);
+rect(205,205,125,125);
+rect(330,330,125,125);
+triangle(330,330,330,205,455,330);
+triangle(455,330,455,455,580,455);
+
+//piece violette
+
+fill('rgba(88,0,138,0.8)');
+triangle(205,330,330,205,330,330);
+triangle(205,330,330,455,330,330);
+rect(330,330,125,125);
+triangle(330,330,330,205,455,330);
+triangle(455,330,455,455,580,455); */
