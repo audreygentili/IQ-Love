74fa9bfe320d1b45a816a496bae90bd8e73a25f0
Theo_Dalban <theo.dalban@steady-sun.com>
2022-11-08 19:39:32 +0100

    Création du pattern / solution de test

---
+++ solution.js
@@ -0,0 +1,3 @@
+var solution81 = new Solution(81,[205,330,"o",true],[205,205,"e",false],[330,580,"e",false],
+[580,205,"n",false],[455,80,"e",true],[80,455,"e",false],[455,330,"n",false],
+[205,80,"e",true],[580,580,"e",false],[705,330,"o",false]);
